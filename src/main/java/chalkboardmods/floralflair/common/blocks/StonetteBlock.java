89b0a7a614a46893b728c42471e48eb1c8a5dbd6
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-06-19 22:32:08 -0500

    Updated to 1.19
    Cleaned up code
    Formatted code
    Removed unnecessary comments
    Added missing lang entry

--- src/main/java/chalkboardmods/floralflair/common/blocks/StonetteBlock.java
+++ src/main/java/chalkboardmods/floralflair/common/blocks/StonetteBlock.java
@@ -1,7 +1,6 @@
 package chalkboardmods.floralflair.common.blocks;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.tag.BlockTags;
@@ -11,17 +10,20 @@ import net.minecraft.world.WorldView;
 
 public class StonetteBlock extends FlowerBlock {
 
-    public StonetteBlock(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
-        super(suspiciousStewEffect, effectDuration, settings);
-    }
-    @Override
-    protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
-        return floor.isIn(BlockTags.BASE_STONE_OVERWORLD);
-    }
-    @Override
-    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
-        BlockPos blockPos = pos.down();
-        BlockState blockState = world.getBlockState(blockPos);
-        return blockState.isIn(BlockTags.BASE_STONE_OVERWORLD);
-    }
+	public StonetteBlock(StatusEffect suspiciousStewEffect, int effectDuration, Settings settings) {
+		super(suspiciousStewEffect, effectDuration, settings);
+	}
+
+	@Override
+	protected boolean canPlantOnTop(BlockState floor, BlockView world, BlockPos pos) {
+		return floor.isIn(BlockTags.BASE_STONE_OVERWORLD);
+	}
+
+	@Override
+	public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+		BlockPos blockPos = pos.down();
+		BlockState blockState = world.getBlockState(blockPos);
+		return blockState.isIn(BlockTags.BASE_STONE_OVERWORLD);
+	}
+
 }
--- src/main/java/chalkboardmods/floralflair/core/FloralConfig.java
+++ src/main/java/chalkboardmods/floralflair/core/FloralConfig.java
@@ -7,88 +7,100 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-
 @Config(name = FloralFlair.MOD_ID)
 public class FloralConfig implements ConfigData {
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Pulse Petal flowers generating in forests [default = 8]")
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    public int pulsePetalDensity = 8;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Foxnip flowers generating in taigas [default = 8]")
-    public int foxnipDensity = 8;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("Frosted Foxnip flower spawning density in game [default = 10]")
-    public int frostedFoxnipDensity = 10;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Fairy Blossom flowers generating in flower forests [default = 10]")
-    public int fairyBlossomDensity = 10;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Gem of the Jungle flowers in jungles [default = 12]")
-    public int jungleGemDensity = 12;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Rose flowers in meadows [default = 8]")
-    public int roseDensity = 8;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @Comment("The density of Muscari flowers in snowy places [default = 10]")
-    public int muscariDensity = 10;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Sunset Poppy flowers in savannas [default = 10]")
-    public int sunsetPoppyDensity = 10;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Purpureum flowers in swamps [default = 12]")
-    public int purpureumDensity = 12;
-    // Midnight_orchid
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Midnight Orchid flowers [default = 12]")
-    public int midnightOrchidDensity = 12;
-    // Hyacinth
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Hyacinth flowers [default = 10]")
-    public int hyacinthDensity = 10;
-    // Anthurium
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Anthurium flowers [default = 8]")
-    public int anthuriumDensity = 8;
-    // Snowfall flower
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Snow Fall flowers [default = 10]")
-    public int snowFallFlowerDensity = 10;
-    // Lunula
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Spiked Tulip flowers [default = 3]")
-    public int spikedTulipDensity = 3;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Lunula flowers [default = 10]")
-    public int lunulaDensity = 10;
-    // Stonnete
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Stonnete flowers [default = 3]")
-    public int stonneteDensity = 3;
-    @ConfigEntry.BoundedDiscrete(max = 24L)
-    @ConfigEntry.Gui.RequiresRestart
-    @Comment("The density of Scilla flowers [default = 10]")
-    public int scillaDensity = 10;
 
-    public static void init() {
-        AutoConfig.register(FloralConfig.class, JanksonConfigSerializer::new);
-    }
-    public static FloralConfig get() {
-        return AutoConfig.getConfigHolder(FloralConfig.class).getConfig();
-    }
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Pulse Petal flowers generating in forests [default = 8]")
+	public int pulsePetalDensity = 8;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Foxnip flowers generating in taigas [default = 8]")
+	public int foxnipDensity = 8;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("Frosted Foxnip flower spawning density in game [default = 10]")
+	public int frostedFoxnipDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Fairy Blossom flowers generating in flower forests [default = 10]")
+	public int fairyBlossomDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Gem of the Jungle flowers in jungles [default = 12]")
+	public int jungleGemDensity = 12;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Rose flowers in meadows [default = 8]")
+	public int roseDensity = 8;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Muscari flowers in snowy places [default = 10]")
+	public int muscariDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Sunset Poppy flowers in savannas [default = 10]")
+	public int sunsetPoppyDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Purpureum flowers in swamps [default = 12]")
+	public int purpureumDensity = 12;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Midnight Orchid flowers [default = 12]")
+	public int midnightOrchidDensity = 12;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Hyacinth flowers [default = 10]")
+	public int hyacinthDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Anthurium flowers [default = 8]")
+	public int anthuriumDensity = 8;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Snow Fall flowers [default = 10]")
+	public int snowFallFlowerDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Spiked Tulip flowers [default = 3]")
+	public int spikedTulipDensity = 3;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Lunula flowers [default = 10]")
+	public int lunulaDensity = 10;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Stonnete flowers [default = 3]")
+	public int stonneteDensity = 3;
+
+	@ConfigEntry.Gui.RequiresRestart
+	@ConfigEntry.BoundedDiscrete(max = 24)
+	@Comment("The density of Scilla flowers [default = 10]")
+	public int scillaDensity = 10;
+
+	public static void init() {
+		AutoConfig.register(FloralConfig.class, JanksonConfigSerializer::new);
+	}
+
+	public static FloralConfig get() {
+		return AutoConfig.getConfigHolder(FloralConfig.class).getConfig();
+	}
 
 }
